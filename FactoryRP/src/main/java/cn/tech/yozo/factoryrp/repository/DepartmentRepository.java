2c6025c333acc600d9ce9c5275a795d40b1396bb
chenxiang <chenxiang_cd@keruyun.com>
2017-11-16 21:21:55 +0800

    增加部门查询列表接口

---
+++ FactoryRP/src/main/java/cn/tech/yozo/factoryrp/repository/DepartmentRepository.java
@@ -0,0 +1,21 @@
+package cn.tech.yozo.factoryrp.repository;
+
+import cn.tech.yozo.factoryrp.entity.Department;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * 部门信息数据处理
+ */
+@Repository
+public interface DepartmentRepository extends BaseRepository<Department,Long>{
+
+    /**
+     * 根据企业标识和数据有效性查询部门列表
+     * @param corporateIdentify
+     * @param statusFlag
+     * @return
+     */
+    public List<Department> findByCorporateIdentifyAndStatusFlag(Long corporateIdentify,Integer statusFlag);
+}
