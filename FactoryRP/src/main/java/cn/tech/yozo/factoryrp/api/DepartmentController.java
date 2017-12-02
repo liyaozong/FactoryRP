90942696fb9ceabd2f1458550b640030e7a94df8
chenxiang <chenxiang_cd@keruyun.com>
2017-12-02 15:48:45 +0800

    修改部门接口

--- FactoryRP/src/main/java/cn/tech/yozo/factoryrp/api/DepartmentController.java
+++ FactoryRP/src/main/java/cn/tech/yozo/factoryrp/api/DepartmentController.java
@@ -65,8 +65,9 @@ public class DepartmentController extends BaseController{
     @RequestMapping("deleteDept")
     @ApiImplicitParams(@ApiImplicitParam(paramType = "query",dataType = "Long",name = "id",
             value = "需要删除部门主键",required = true,defaultValue = "6"))
-    public void deleteDept(Long id){
+    public ApiResponse deleteDept(Long id){
         departmentService.delete(id);
+        return apiResponse();
     }
 
     @ApiOperation(value = "调整上级部门",notes = "调整上级部门",httpMethod = "GET")
@@ -74,7 +75,8 @@ public class DepartmentController extends BaseController{
     @ApiImplicitParams({@ApiImplicitParam(paramType = "query",dataType = "Long",name = "id",
             value = "需要调整都部门主键",required = true,defaultValue = "1"),@ApiImplicitParam(paramType = "query",dataType = "Long",name = "parentId",
             value = "上级部门ID",required = true,defaultValue = "2")})
-    public void updateUpLevel(Long id,Long parentId){
+    public ApiResponse updateUpLevel(Long id,Long parentId){
         departmentService.updateUpLevel(id,parentId);
+        return apiResponse();
     }
 }
