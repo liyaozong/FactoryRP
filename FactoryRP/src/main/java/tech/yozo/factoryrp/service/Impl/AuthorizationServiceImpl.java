cbee1e5267b61cd35d54ae0d0f92430f0f70c234
wanghao <313402703@qq.com>
2018-01-16 19:57:19 +0800

    bug修改~

--- FactoryRP/src/main/java/tech/yozo/factoryrp/service/Impl/AuthorizationServiceImpl.java
+++ FactoryRP/src/main/java/tech/yozo/factoryrp/service/Impl/AuthorizationServiceImpl.java
@@ -316,6 +316,10 @@ public class AuthorizationServiceImpl implements AuthorizationService {
 
         User user = userRepository.findByUserIdAndCorporateIdentify(userRoleReq.getUserId(), corporateIdentify);
 
+        if(CheckParam.isNull(user)){
+            throw new BussinessException(ErrorCode.CORPORATE_USER__NOTEXIST_ERROR.getCode(),ErrorCode.CORPORATE_USER__NOTEXIST_ERROR.getMessage());
+        }
+
         List<UserRole> userRoles = new ArrayList<>();
         roleIdList.stream().forEach(r1 -> {
             UserRole userRole = new UserRole();
--- FactoryRP/src/main/java/tech/yozo/factoryrp/utils/ErrorCode.java
+++ FactoryRP/src/main/java/tech/yozo/factoryrp/utils/ErrorCode.java
@@ -37,7 +37,8 @@ public enum ErrorCode {
     CURRENTUSER_OPERATESELF_ERROR("0000019","菜单不存在"),
     PROCESS_NAME_REPET_ERROR("0000020","流程已经存在"),
     PROCESS_NOT_EXIST_ERROR("0000021","流程未被定义，开启流程失败"),
-    PROCESS_INSTANCE_NOT_EXIST_ERROR("0000022","流程实例不存在，开启流程失败");
+    PROCESS_INSTANCE_NOT_EXIST_ERROR("0000022","流程实例不存在，开启流程失败"),
+    CORPORATE_USER__NOTEXIST_ERROR("0000023","用户不存在");
 
 
     private String code;
