af95e71d096a1666159f375657e00ec4f04c923c
liyaozong <liyaozong@aliyun.com>
2018-02-27 18:41:24 +0800

    点检、保养任务示例代码

--- android/FactoryRP/app/src/main/java/tech/yozo/factoryrp/ui/fragment/WorkBenchFragment.java
+++ android/FactoryRP/app/src/main/java/tech/yozo/factoryrp/ui/fragment/WorkBenchFragment.java
@@ -7,24 +7,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.*;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
 import tech.yozo.factoryrp.R;
-import tech.yozo.factoryrp.ui.DeviceAddActivity;
-import tech.yozo.factoryrp.ui.RepairAddActivity;
-import tech.yozo.factoryrp.ui.RepairRecordListActivity;
-import tech.yozo.factoryrp.ui.ReportFaultActivity;
+import tech.yozo.factoryrp.ui.*;
 import tech.yozo.factoryrp.utils.HttpClient;
 import tech.yozo.factoryrp.vo.resp.device.trouble.WorkOrderCountVo;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 
 /**
@@ -39,7 +33,9 @@ public class WorkBenchFragment extends BaseFragment implements HttpClient.OnHttp
     private static final String ARG_PARAM2 = "id";
 
     private static final int UPDATE_COUNT = 110;
+    private static final int UPDATE_INSPECT_TASK = 111;
 
+    Unbinder unbinder;
     @BindView(R.id.button_ask_repair)
     Button buttonAskRepair;
     @BindView(R.id.button_add_repair)
@@ -54,18 +50,21 @@ public class WorkBenchFragment extends BaseFragment implements HttpClient.OnHttp
     TextView textViewExecuting;
     @BindView(R.id.textView_waitto_verify)
     TextView textViewWaittoVerify;
-    @BindView(R.id.listview_maintain_task)
-    ListView listviewMaintainTask;
     @BindView(R.id.listview_check_task)
     ListView listviewCheckTask;
-    @BindView(R.id.noNewMaintainTask)
-    TextView noNewMaintainTask;
     @BindView(R.id.noNewCheckTask)
     TextView noNewCheckTask;
-    Unbinder unbinder;
+    @BindView(R.id.tv_mtask_today)
+    TextView tvMtaskToday;
+    @BindView(R.id.tv_mtask_tomorrow)
+    TextView tvMtaskTomorrow;
+    @BindView(R.id.tv_mtask_overdue)
+    TextView tvMtaskOverdue;
 
     private int mParam_mode;
     private String mParam_id;
+
+    private List<Map<String, Object>> list;
 
     public WorkBenchFragment() {
         // Required empty public constructor
@@ -103,8 +102,30 @@ public class WorkBenchFragment extends BaseFragment implements HttpClient.OnHttp
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_workbench, container, false);
         unbinder = ButterKnife.bind(this, view);
-        listviewMaintainTask.setEmptyView(noNewMaintainTask);
+
+
         listviewCheckTask.setEmptyView(noNewCheckTask);
+        String[] from = {"title", "time"};
+        int[] to = {R.id.et_mtask_name, R.id.et_mtask_start_time};
+        list = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("title", "任务：" + i);
+            map.put("time",
+                    new SimpleDateFormat("计划开始时间：yyyy年MM月dd日").format(new Date()));
+            list.add(map);
+        }
+        SimpleAdapter adapter = new SimpleAdapter(getContext(), list, R.layout.item_inspect_list, from, to);
+        listviewCheckTask.setAdapter(adapter);
+        listviewCheckTask.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //TODO
+                Intent intent = new Intent(getActivity(), InspectDeviceActivity.class);
+                startActivityForResult(intent, UPDATE_INSPECT_TASK);
+            }
+        });
+
         return view;
     }
 
@@ -124,7 +145,9 @@ public class WorkBenchFragment extends BaseFragment implements HttpClient.OnHttp
         HttpClient.getInstance().requestTroubleCount(getContext(), this);
     }
 
-    @OnClick({R.id.button_ask_repair, R.id.button_add_repair, R.id.button_add_device, R.id.textView_waitto_audit, R.id.textView_waitto_exec, R.id.textView_executing, R.id.textView_waitto_verify})
+    @OnClick({R.id.button_ask_repair, R.id.button_add_repair, R.id.button_add_device, R.id.textView_waitto_audit,
+            R.id.textView_waitto_exec, R.id.textView_executing, R.id.textView_waitto_verify,
+            R.id.tv_mtask_today, R.id.tv_mtask_tomorrow, R.id.tv_mtask_overdue})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.button_ask_repair: {
@@ -166,12 +189,30 @@ public class WorkBenchFragment extends BaseFragment implements HttpClient.OnHttp
                 startActivityForResult(intent, UPDATE_COUNT);
                 break;
             }
+            case R.id.tv_mtask_today: {
+                Intent intent = new Intent(getContext(), RepairRecordListActivity.class);
+                intent.putExtra(RepairRecordListActivity.RECORD_CATEGORY, HttpClient.REQUEST_TROUBLE_REPAIRING);
+                startActivityForResult(intent, UPDATE_COUNT);
+                break;
+            }
+            case R.id.tv_mtask_tomorrow: {
+                Intent intent = new Intent(getContext(), RepairRecordListActivity.class);
+                intent.putExtra(RepairRecordListActivity.RECORD_CATEGORY, HttpClient.REQUEST_TROUBLE_REPAIRING);
+                startActivityForResult(intent, UPDATE_COUNT);
+                break;
+            }
+            case R.id.tv_mtask_overdue: {
+                Intent intent = new Intent(getContext(), RepairRecordListActivity.class);
+                intent.putExtra(RepairRecordListActivity.RECORD_CATEGORY, HttpClient.REQUEST_TROUBLE_REPAIRING);
+                startActivityForResult(intent, UPDATE_COUNT);
+                break;
+            }
         }
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == UPDATE_COUNT && resultCode == Activity.RESULT_OK) {
+        if (requestCode == UPDATE_COUNT && resultCode == Activity.RESULT_OK) {
             HttpClient.getInstance().requestTroubleCount(getContext(), this);
         }
     }
@@ -182,25 +223,25 @@ public class WorkBenchFragment extends BaseFragment implements HttpClient.OnHttp
                 WorkOrderCountVo mTroubleCount = (WorkOrderCountVo) obj;
                 if (mTroubleCount != null) {
                     textViewWaittoAudit.setText(String.valueOf(mTroubleCount.getWaitAuditNum()));
-                    if(mTroubleCount.getWaitAuditNum() <= 0) {
+                    if (mTroubleCount.getWaitAuditNum() <= 0) {
                         textViewWaittoAudit.setEnabled(false);
                     } else {
                         textViewWaittoAudit.setEnabled(true);
                     }
                     textViewWaittoExec.setText(String.valueOf(mTroubleCount.getWaitRepairNum()));
-                    if(mTroubleCount.getWaitRepairNum() <= 0) {
+                    if (mTroubleCount.getWaitRepairNum() <= 0) {
                         textViewWaittoExec.setEnabled(false);
                     } else {
                         textViewWaittoExec.setEnabled(true);
                     }
                     textViewExecuting.setText(String.valueOf(mTroubleCount.getRepairingNum()));
-                    if(mTroubleCount.getRepairingNum() <= 0) {
+                    if (mTroubleCount.getRepairingNum() <= 0) {
                         textViewExecuting.setEnabled(false);
                     } else {
                         textViewExecuting.setEnabled(true);
                     }
                     textViewWaittoVerify.setText(String.valueOf(mTroubleCount.getWaitValidateNum()));
-                    if(mTroubleCount.getWaitValidateNum() <= 0) {
+                    if (mTroubleCount.getWaitValidateNum() <= 0) {
                         textViewWaittoVerify.setEnabled(false);
                     } else {
                         textViewWaittoVerify.setEnabled(true);
